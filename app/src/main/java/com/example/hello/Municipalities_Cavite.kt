0206422f9485f46ad19b46cf67eaf672eef4831a
Rphlow <ezekielpramosraphael@gmail.com>
2024-07-28 05:15:14 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/deploymentTargetDropDown.xml
    #	app/src/main/java/com/example/hello/CAR.kt
    #	app/src/main/java/com/example/hello/region1.kt
    #	app/src/main/java/com/example/hello/region2.kt
    #	app/src/main/java/com/example/hello/region3.kt
    #	app/src/main/java/com/example/hello/region4A.kt
    #	app/src/main/java/com/example/hello/region4B.kt
    #	app/src/main/java/com/example/hello/region5.kt

--- app/src/main/java/com/example/hello/Municipalities_Cavite.kt
+++ app/src/main/java/com/example/hello/Municipalities_Cavite.kt
@@ -33,136 +33,126 @@ class Municipalities_Cavite: AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val cities = listOf(
-            "Adams",
-            "Bacarra",
-            "Badoc",
-            "Bangui",
-            "Banna",
-            "Burgos",
-            "Carasi",
-            "Currimao",
-            "Dingras",
-            "Dumalneg",
-            "Marcos",
-            "Nueva Era",
-            "Pagudpud",
-            "Paoay",
-            "Pasuquin",
-            "Piddig",
-            "Pinili",
-            "San Nicolas",
-            "Sarrat",
-            "Solsona",
-            "Vintar"
+            "Alfonso",
+            "Amadeo",
+            "General Emilio Aguinaldo",
+            "General Mariano Alvarez",
+            "Indang",
+            "Kawit",
+            "Magallanes",
+            "Maragondon",
+            "Mendez",
+            "Naic",
+            "Rosario",
+            "Silang",
+            "Tanza",
+            "Ternate",
+
         )
         cityAdapter = CityAdapter(cities) { city ->
             val intent = Intent(this, CityIlocosNorteDetail::class.java)
             val message = when (city) {
-                "Adams"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Bacarra"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Badoc"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Bangui"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Banna"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Burgos"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Carasi"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Currimao"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Dingras"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Dumalneg"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Marcos"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Nueva Era"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Pagudpud"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Paoay"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Pasuquin"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Piddig"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Pinili"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "San Nicolas"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Sarrat"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Solsona"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
-                "Vintar"-> "Emergency Numbers\n " +
-                        "Telephone Number:\n 9-234-13\n 4-215-32\n " +
-                        "Mobile Number:\n 098765412 ( Smart )\n 097612567 ( Globe )\n" +
-                        "Police Number: 112\n" +
-                        " NDRRMC: 231"
+                "Alfonso" -> """
+                    MDRRMO: 046 443 9083
+                    BFP: 0915 602 2113, 046 522 0480
+                    PNP: 0906 536 6135, 0998 967 3364
+                    Hospital: 0917 306 2419, 415 0190
+                    
+                """.trimIndent()
+                "Amadeo" -> """
+                    MDRRMO: 0917-504-7419
+                    BFP: (046) 483-2490, 0915-601-6805
+                    PNP:  (046) 483-3022, 0965-199-3111
+                  
+                    
+                """.trimIndent()
+                "General Emilio Aguinaldo" -> """
+             
+                    BFP: (046) 487-2697
+                    PNP: 0998 967 3512, 0998 598 5616
+                    
+                    
+                """.trimIndent()
+                "General Mariano Alvarez" -> """
+                    MDRRMO:(046) 443-9839
+                    BFP: (046)443-9110, 09071784300
+                    PNP: 0956-882-3167, 0998-446-0494
+                    Municipal Health Office: 0951-669-0285, 0951-669-0286
+                    
+                """.trimIndent()
+                "Indang" -> """
+                    LDRRMO: 0915 206 6929, 0916 183 1458
+                    BFP: 0915 603 4245, 415 1217
+                    PNP:  0949 184 9145
+                    Health Center: 415 0286, 683 9772
+                    
+                    
+                """.trimIndent()
+                "Kawit" -> """
+                    MDRRMO: (046) 440 - 0722
+                    BFP: (046) 484-5250
+                    PNP: 0998 967 3356, 0998 598 5607
+                    
+                    
+                """.trimIndent()
+                "Magallanes" -> """
+                    MDRRMO: 0930 709 7811
+                    BFP: (046) 4127274, 0915 602 1644
+                    PNP: 0998 967 3368, 0998 598 5618
+                 
+                    
+                    
+                """.trimIndent()
+                "Maragondon" -> """
+                    MDRRMO: 0906 568 0500
+                    BFP: (046) 412-1911, 0936 088 2538
+                    PNP: (046) 412 1161, 0998 598 5619 
+                    
+                    
+                """.trimIndent()
+                "Mendez" -> """
+                    MDRRMO: (046) 443-0209, 09568820987
+                    BFP: (046) 482-0712, 0919092 0206
+                    PNP: (046) 443-9174, 0998 967 3370, 0998 598 5620
+                    
+                    
+                """.trimIndent()
+                "Naic" -> """
+                    MDRRMO: 0918 967 9772
+                    BFP: (046) 412-1481, 0956-483 0226
+                    PNP: 0998 967 3371, 0998 598 5621
+             
+                """.trimIndent()
+                "Rosario" -> """
+                    MDRRMO: (046) 432 0526, 0969 011 6787, 0945 655 5002
+                    BFP: (046) 438-1616, 0935 462 2522
+                    PNP: 0998 967 3358,  0998 598 5609
+                  
+                    
+                    
+                """.trimIndent()
+                "Silang" -> """
+                    MDRRMO: (046)- 414-3776, 0935-601-6738, 0995-987-8996
+                    BFP:  (046) 414-0484,  0995 961 6108
+                    PNP: (046)- 414-0204, 0995-987-8996
+                   
+                    
+                """.trimIndent()
+                "Tanza" -> """
+                    MDRRMO: 0977 345 5035, 0928 757 9030
+                    BFP: (046) 505-6084, 0968 347 9894
+                    PNP: (046) 437-6558, 0917 850 2555
+                 
+                    
+                """.trimIndent()
+                "Ternate" -> """
+                    MDRRMO: 0915 799 4206
+                    BFP: 0905 252 4246
+                    PNP: 0998 967 3374, 0998 598 5623
+                    
+                    
+                """.trimIndent()
+
                 else -> "Default message"
             }
             intent.putExtra("MESSAGE", message)
